e08c10e7391c60365c228a3afb1d6a1528166f8f
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-08-01 11:59:19 +0530

    Configured login logout controllers

--- controllers/main.js
+++ controllers/main.js
@@ -4,10 +4,51 @@ const fs = require("fs");
 
 exports.login = async (request, response, next) => {
 
-    response.send({
-        'hi': 'Hello'
-    });
+    const { username, password } = request.body;
+    // console.log(request)
+    console.log('Post request');
+    if (username && password) {
+        console.log(username, password)
+        try {
+            User.findOne({ where: { username } }).then((user) => {
+                if (user && user.matchPassword(password)) {
+                    request.session.userId = user.id;
+                    response.send({
+                        'hi': 'loggedin'
+                    });
+                } else {
+                    response.send({
+                        'message': 'Invalid Credentials'
+                    });
+                }
+            });
+        } catch (error) {
+            response.send({
+                'hi': error
+            });
+        }
+
+    } else {
+        response.send({
+            'hi': 'no'
+        });
+    }
 };
+
+
+exports.logout = async (request, response, next) => {
+    request.session.destroy((error) => {
+        if (error) {
+            return response.redirect('/admin/dashboard');
+        }
+
+        // After destroying the session in the server memory
+        // Clear the cookie on client
+        response.clearCookie(process.env.SESSION_NAME || 'sid');
+        // Then redirect to login page
+        response.redirect('/admin/login')
+    });
+}
 
 exports.campusName = async (request, response, next) => {
 
--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { login, campusName } = require('../controllers/main');
+const { login,  logout, campusName } = require('../controllers/main');
 
-router.route('/login').get(login);
+router.route('/login').post(login);
+router.route('/logout').post(logout);
 router.route('/campusname').get(campusName);
 // router.route('/upload').get(upload).post(uploadMiddleware.single('file'), upload);
 // router.route('/blog').get(blog);
