3bcb87cd12e0d392ca1f50dc58af2de1694054af
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-08-07 13:43:18 +0530

    Configured few endpoints

--- controllers/simulation.js
+++ controllers/simulation.js
@@ -1,9 +1,61 @@
 const User = require('../models/User');
 
+exports.policyPlanner = async (request, response, next) => {
+
+    response.send({
+        "GeneralInput":
+        {
+            "No_of_days_to_simulate": [1, 2, 3, 4],
+            "Day_Resolution_By_Hours": [3, 6, 5, 4]
+        },
+        "EpidemicParameterInput":
+        {
+            "InfectionRate": 0.5,
+            "RecoveryRate": [0.25, 0.5, 0.75, 1],
+            "Probability_of_Showing_Symptoms": [0.25, 0.5, 0.75, 1],
+            "Pre_SymptomaticPeriod": [1, 2, 3, 4]
+        },
+        "PolicyInput":
+        {
+            "Lockdown_Capacity_of_Each_Building": [0.25, 0.5, 0.75, 1],
+            "Threshold_for_AutoLockdown": [1, 2, 3, 4],
+            "No_of_Campus_Visitors_Allowed": [50, 60, 70, 80],
+            "Daily_Testing_Capacity": [25, 50, 75, 100],
+            "Daily_Testing_Capacity_Increment Rate": [1, 2, 3],
+            "Dorfman_Pool_Size": [25, 50, 75, 100],
+            "Tapestry_Pool_Size": [50, 60, 70, 80],
+            "Tapestry_Pool_Duplication": [1, 2, 3, 4, 5],
+            "Test_Sensitivity": [50, 60, 70, 80],
+            "Test_Specificity": [50, 60, 70, 80],
+            "Vaccine_Coverage": [50, 60, 70, 80],
+            "Vaccine_Efficiency1": [50, 60, 70, 80],
+            "Vaccine_Efficiency2": [50, 60, 70, 80],
+            "Minimum_Inter-Dose_Period": [50, 60, 70, 80],
+            "Hotspot_Threshold": [50, 60, 70, 80],
+            "At-risk_Percentile_Threshold": [50, 60, 70, 80],
+        },
+        "TestingStrategy": {
+            "TestingStrategy": ["Random"]
+        }
+    });
+};
+
 exports.initialization = async (request, response, next) => {
 
     response.send({
-        'hi': 'Hello'
+        "Random":
+        {
+            "No_of_People_Affected": [10, 20, 30, 40],
+            "Day_Resolution_by_Hours": [1, 4, 6, 7]
+        },
+        "TimeSeries":
+        {
+            "No_of_Daily_Cases": [10, 20, 30, 40],
+            "No_of_days_to_Simulate": [10, 20, 30, 40]
+        },
+        "Spatial_Distribution": {
+            "Spatial_Distribution": [10, 20, 30, 40]
+        }
     });
 };
 
--- routes/simulation.js
+++ routes/simulation.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { initialization, saveSimulation, savedSimulations, deleteSavedSimulations, run, save, runSavedSimulations } = require('../controllers/simulation');
+const { protect } = require('../middleware/auth')
 
-router.route('/initialization').get(initialization);
-router.route('/savesimulation').get(saveSimulation);
-router.route('/savedsimulations').get(savedSimulations);
-router.route('/savedsimulations/delete').get(deleteSavedSimulations);
-router.route('/run').get(run);
-router.route('/save').get(save);
-router.route('/savedsimulations/run').get(runSavedSimulations);
+const { policyPlanner, initialization, saveSimulation, savedSimulations, deleteSavedSimulations, run, save, runSavedSimulations } = require('../controllers/simulation');
+
+router.route('/policyplanner').get(protect, policyPlanner);
+router.route('/initialization').get(protect, initialization);
+router.route('/savesimulation').get(protect, saveSimulation);
+router.route('/savedsimulations').get(protect, savedSimulations);
+router.route('/savedsimulations/delete').get(protect, deleteSavedSimulations);
+router.route('/run').get(protect, run);
+router.route('/save').get(protect, save);
+router.route('/savedsimulations/run').get(protect, runSavedSimulations);
 
 // router.route('/upload').get(upload).post(uploadMiddleware.single('file'), upload);
 // router.route('/blog').get(blog);
