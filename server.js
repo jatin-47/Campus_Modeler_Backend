6db82d39b14c019b11a4970d7e4c98083df2565a
jatin-47 <saini.js2001@gmail.com>
2021-09-15 19:00:47 +0530

    cleaned server.js and added static route (with auth)

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-// Third party imports
 require('dotenv').config({ path: './config.env' });
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
-
-// node inbuilt package imports
-const path = require('path');
-
-// filesystem imports
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
-const User = require('./models/User');
-// const models = require('./models'); // import all the models
+const {protect} = require('./middleware/auth');
+const {adminprotect} = require('./middleware/admincheck');
+
 //const createuser = require('./create_user');
 //createuser("jatin", "jatin@gmail.com", "jatin@123", "admin", "kharagpur", "Jatin", "Saini", "Male");
+/* 
+const Counter = require("./models/Counter");
+const CampusNames = require('./config/campusnames');
+for(let campus of CampusNames.enum){
+    Counter.create({
+        campusname : campus,
+        CampusBuilding : 1,
+        User : 1,
+        BatchStudent : 1,
+        Faculty : 1,
+        Staff : 1
+    });
+} 
+*/
 
-//const uploadMiddleware = require('./middleware/uploadFile');
-
-// Connecting to Database
 connectDB();
 
-
-// Creating an express app
 const app = express();
 
 global.__basedir = __dirname;
@@ -35,25 +37,22 @@ const corsOptions = {
     "optionsSuccessStatus": 204,
     "credentials": true
 }
-
-// Configuring cors
 app.use(cors(corsOptions));
 
 // Adding a json middleware for parsing application/json data
 app.use(express.json());
 // For parsing application/xwww-form-urlencoded data
 app.use(express.urlencoded({ extended: true }));
-// For parsing multipart/form-data
-// app.use(upload.array());
-//app.use(uploadMiddleware.any());
 
 // Setting port as a key 'port' to the app
 app.set('port', process.env.PORT || 5050);
+
 
 // Setting up the routes
 app.get('/', (request, response) => {
     response.send('Hello World');
 });
+app.use('/static', protect, adminprotect, express.static(process.env.STATIC_PATH));
 app.use('/campus/main', require('./routes/main'));
 app.use('/campus/simulation', require('./routes/simulation'));
 app.use('/campus/masterdata', require('./routes/masterdata'));
